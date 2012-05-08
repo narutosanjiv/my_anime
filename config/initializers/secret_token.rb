60652ab812405801f066694b126dc3623d76d2fb
Sanjiv Jha <narutosanjiv@gmail.com>
2012-05-08 15:41:12 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyAnime::Application.config.secret_token = '1da2218f28b02c04da7e4836d1cb6721e029c1b46168685f9dc1e8de853f94b6dd4f8e89503a0aa58e74a1af58eada96f009690301fe84f32c0403bf5d68edf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MyAnime::Application.config.session_store :cookie_store, key: '_my_anime_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MyAnime::Application.config.session_store :active_record_store
